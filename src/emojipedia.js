9f063ae4e9c71f28a3b95cbc1325312ccb2c53db
Anuraj Kumar <anurajkumar6294@gmail.com>
2023-01-11 19:14:28 +0530

    emojipedia

---
+++ src/emojipedia.js
@@ -0,0 +1,64 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "🤓",
+      name: "Nerd Face",
+      meaning:
+        "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+    },
+    {
+      id: 5,
+      emoji: "😀",
+      name: "Grinning Face",
+      meaning:"A yellow face with simple, open eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general pleasure and good cheer or humor."
+    },
+    {
+      id: 6,
+      emoji: "😄",
+      name: "Grinning Face with Smiling Eyes",
+      meaning:"A yellow face with smiling eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general happiness and good-natured amusement."
+    },
+    {
+      id: 7,
+      emoji: "😆",
+      name: "Grinning Squinting Face",
+      meaning:"A yellow face with a broad, open smile and scrunched, X-shaped eyes. Often conveys excitement or hearty laughter."
+    },
+    {
+      id: 8,
+      emoji: "😠",
+      name: "Angry Face",
+      meaning:"A yellow face with a frowning mouth and eyes and eyebrows scrunched downward in anger.Conveys varying degrees of anger, from grumpiness and irritation to disgust and outrage. May also represent someone acting tough or being mean."
+    },
+    {
+      id: 9,
+      emoji: "😍",
+      name: "Smiling Face with Heart-Eyes",
+      meaning:"A yellow face with an open smile, sometimes showing teeth, and red, cartoon-styled hearts for eyes. Often conveys enthusiastic feelings of love, infatuation, and adoration, e.g., I love/am in love with this person or thing."
+    }
+    
+  ];
+  
+  export default emojipedia;
+  
